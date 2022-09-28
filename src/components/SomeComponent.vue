431e8f33ef65a048e75653e4999f3922a589b856
Mathieu Marteau <marteau.mathieu@gmail.com>
2022-09-28 14:11:27 +0200

    Initial Commit

---
+++ src/components/SomeComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div v-for="i in 10000">
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </div>
+    <div v-for="i in 5">
+      Hello World!
+    </div>
+  </div>
+</template>
